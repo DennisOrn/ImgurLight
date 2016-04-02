34df28f2902f95c802f0e515b3051efdac911f04
SoEgd <SoEgd@users.noreply.github.com>
2016-04-02 16:11:42 +0200

    first commit

---
+++ ImgurLight/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ImgurLight
+//
+//  Created by Dennis Örnberg on 02/04/16.
+//  Copyright © 2016 Dennis Örnberg. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
